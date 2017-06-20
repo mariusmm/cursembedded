e69552c35226ab5e388cdf428a8caea5e6fb3238
mariusmonton <marius.monton@gmail.com>
2017-06-20 12:53:07 +0200

    Fixed

--- GPIO_1/src/GPIO_1.c
+++ GPIO_1/src/GPIO_1.c
@@ -3,28 +3,25 @@
 #include "em_gpio.h"
 #include "em_cmu.h"
 
+int main(void) {
+	/* Chip errata */
+	CHIP_Init();
 
-int main(void)
-{
-  /* Chip errata */
-  CHIP_Init();
+	CMU_ClockEnable(cmuClock_GPIO, true);
 
-  CMU_ClockEnable(cmuClock_GPIO, true);
+	GPIO_PinModeSet(gpioPortD, 7, gpioModePushPullDrive, 0); /* LED */
+	GPIO_PinModeSet(gpioPortD, 8, gpioModeInput, 0); /* Boto 0 */
+	GPIO_PinModeSet(gpioPortB, 11, gpioModeInput, 0); /* Boto 1 */
 
+	/* Infinite loop */
+	while (1) {
+		if (GPIO_PinInGet(gpioPortD, 8) == 0) {
+			GPIO_PinOutClear(gpioPortD, 7);
+		}
 
-  GPIO_PinModeSet(gpioPortD, 7, gpioModePushPullDrive, 0);	/* LED */
-  GPIO_PinModeSet(gpioPortD, 8, gpioModeInput, 0);		/* Boto 0 */
-  GPIO_PinModeSet(gpioPortB, 11, gpioModeInput, 0);		/* Boto 1 */
+		if (GPIO_PinInGet(gpioPortB, 11) == 0) {
+			GPIO_PinOutSet(gpioPortD, 7);
+		}
 
-  /* Infinite loop */
-  while (1) {
-
-	  if (GPIO_PinInGet(gpioPortD, 8) == 0) {
-		  GPIO_PinOutClear(gpioPortD, 7);
-	  }
-
-	  if (GPIO_PinInGet(gpioPortB, 11) == 0) {
-		  GPIO_PinOutSet(gpioPortD, 7);
-	  }
-  }
+	}
 }
