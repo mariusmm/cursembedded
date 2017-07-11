a9fdf29b3adb7b8f91869ee36e5719d7ac563c70
mariusmonton <marius.monton@gmail.com>
2017-07-12 00:01:53 +0200

    Added Timer 1 example

---
+++ Simplicity/Timer_1/src/Timer_1.c
@@ -0,0 +1,67 @@
+/**
+ ******************************************************************************
+ * @file    Timer_1.c
+ * @author  Màrius Montón <marius.monton@gmail.com>
+ * @version V1.0
+ * @date    05-July-2017
+ * @brief   Basic Timer management
+ * @license This project is released under the GNU Public License.s
+ * ******************************************************************************
+ */
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_timer.h"
+#include "em_gpio.h"
+#include "em_cmu.h"
+
+#define TOP_VALUE 13671
+
+TIMER_Init_TypeDef timerInit =
+  {
+    .enable     = true,
+    .debugRun   = false,
+    .prescale   = timerPrescale1024,
+    .clkSel     = timerClkSelHFPerClk,
+    .fallAction = timerInputActionNone,
+    .riseAction = timerInputActionNone,
+    .mode       = timerModeUp,
+    .dmaClrAct  = false,
+    .quadModeX4 = false,
+    .oneShot    = true,
+    .sync       = false,
+  };
+
+
+int main(void)
+{
+  /* Chip errata */
+  CHIP_Init();
+
+  /* Enable clks to peripherals */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+  CMU_ClockEnable(cmuClock_TIMER0, true);
+
+  /* Set I/O */
+  GPIO_PinModeSet(gpioPortD, 7, gpioModePushPullDrive, 0); 	/* LED */
+  GPIO_PinModeSet(gpioPortD, 8, gpioModeInput, 0); 			/* Boto 0 */
+  GPIO_PinModeSet(gpioPortB, 11, gpioModeInput, 0); 		/* Boto 1 */
+
+  /* Switch off LED */
+  GPIO_PinOutClear(gpioPortD, 7);
+
+  /* Infinite loop */
+  while (1) {
+
+	  /* If user push button 0, start timer 0 */
+	  if (GPIO_PinInGet(gpioPortD, 8) == 0) {
+		  TIMER_Init(TIMER0, &timerInit);
+	  }
+
+	  /* If timer count gets to TOP_VALUE, toggle LED and stop Timer */
+	  if (TIMER_CounterGet(TIMER0) >= TOP_VALUE) {
+		  GPIO_PinOutToggle(gpioPortD, 7);
+		  TIMER_Reset(TIMER0);
+	  }
+  }
+}
