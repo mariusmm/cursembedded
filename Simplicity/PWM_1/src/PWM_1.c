455f45577231c2173c6789ad65c95a9abb768fec
mariusmonton <marius.monton@gmail.com>
2017-07-12 11:13:07 +0200

    typo in header

--- Simplicity/PWM_1/src/PWM_1.c
+++ Simplicity/PWM_1/src/PWM_1.c
@@ -5,7 +5,7 @@
  * @version V1.0
  * @date    05-July-2017
  * @brief   Using PWM example
- * @license This project is released under the GNU Public License.s
+ * @license This project is released under the GNU Public License.
  * ******************************************************************************
  */
 
