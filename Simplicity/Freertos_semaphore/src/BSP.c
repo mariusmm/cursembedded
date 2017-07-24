c69661cda46639469fea4009afb9eace8f0b43d8
mariusmonton <marius.monton@gmail.com>
2017-07-25 01:27:51 +0200

    Added FreeRTOS semaphore example

---
+++ Simplicity/Freertos_semaphore/src/BSP.c
@@ -0,0 +1,119 @@
+/**
+ ******************************************************************************
+ * @file    BSP.c
+ * @author  Màrius Montón <marius.monton@gmail.com>
+ * @version V1.0
+ * @date    25-July-2017
+ * @brief   Basic BSP enabling printf
+ * @license This project is released under the GNU Public License.s
+ * ******************************************************************************
+ */
+
+#include "BSP.h"
+
+/**
+ * @brief Initialize SWO to be used by printf
+ */
+static void setupSWOForPrint(void) {
+	/* Enable GPIO clock. */
+	CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+
+	/* Enable Serial wire output pin */
+	GPIO->ROUTE |= GPIO_ROUTE_SWOPEN;
+
+#if defined(_EFM32_GIANT_FAMILY) || defined(_EFM32_LEOPARD_FAMILY) || defined(_EFM32_WONDER_FAMILY)
+	/* Set location 0 */
+	GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK)) | GPIO_ROUTE_SWLOCATION_LOC0;
+
+	/* Enable output on pin - GPIO Port F, Pin 2 */
+	GPIO->P[5].MODEL &= ~(_GPIO_P_MODEL_MODE2_MASK);
+	GPIO->P[5].MODEL |= GPIO_P_MODEL_MODE2_PUSHPULL;
+#else
+	/* Set location 1 */
+	GPIO->ROUTE = (GPIO->ROUTE & ~(_GPIO_ROUTE_SWLOCATION_MASK))
+			| GPIO_ROUTE_SWLOCATION_LOC1;
+	/* Enable output on pin */
+	GPIO->P[2].MODEH &= ~(_GPIO_P_MODEH_MODE15_MASK);
+	GPIO->P[2].MODEH |= GPIO_P_MODEH_MODE15_PUSHPULL;
+#endif
+
+	/* Enable debug clock AUXHFRCO */
+	CMU->OSCENCMD = CMU_OSCENCMD_AUXHFRCOEN;
+
+	/* Wait until clock is ready */
+	while (!(CMU->STATUS & CMU_STATUS_AUXHFRCORDY))
+		;
+
+	/* Enable trace in core debug */
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	ITM->LAR = 0xC5ACCE55;
+	ITM->TER = 0x0;
+	ITM->TCR = 0x0;
+	TPI->SPPR = 2;
+	TPI->ACPR = 0xf;
+	ITM->TPR = 0x0;
+	DWT->CTRL = 0x400003FE;
+	ITM->TCR = 0x0001000D;
+	TPI->FFCR = 0x00000100;
+	ITM->TER = 0x1;
+}
+
+/**
+ * @brief write function to be used by printf
+ */
+int _write(int file, const char *ptr, int len) {
+	int x;
+	for (x = 0; x < len; x++) {
+		ITM_SendChar(*ptr++);
+	}
+	return (len);
+}
+
+/**
+ * @brief Init LED GPIO
+ */
+static void BSP_LedInit(void) {
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	GPIO_PinModeSet(gpioPortD, 7, gpioModePushPullDrive, 0); /* LED */
+}
+
+void LedOn(void) {
+	GPIO_PinOutSet(gpioPortD, 7);
+}
+
+void LedOff(void) {
+	GPIO_PinOutClear(gpioPortD, 7);
+}
+
+void LedToggle(void) {
+	GPIO_PinOutToggle(gpioPortD, 7);
+}
+
+/**
+ * @brief Init buttons GPIOs
+ *
+ * This init function enables IRQs and sets priority to 6
+ */
+static void BSP_ButtonsInit() {
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	GPIO_PinModeSet(gpioPortD, 8, gpioModeInput, 0); /* Boto 0 */
+	GPIO_PinModeSet(gpioPortB, 11, gpioModeInput, 0); /* Boto 1 */
+
+	/* Set Interrupt configuration for both buttons */
+	GPIO_IntConfig(gpioPortD, 8, false, true, true);
+	GPIO_IntConfig(gpioPortB, 11, false, true, true);
+
+	/* Set priority to interrupts  */
+	NVIC_SetPriority(GPIO_EVEN_IRQn, 6);
+	NVIC_SetPriority(GPIO_ODD_IRQn, 6);
+
+	/* Enable interrupts */
+	NVIC_EnableIRQ(GPIO_EVEN_IRQn);
+	NVIC_EnableIRQ(GPIO_ODD_IRQn);
+}
+
+void BSP_Init(void) {
+	BSP_LedInit();
+	BSP_ButtonsInit();
+	setupSWOForPrint();
+}
---
+++ Simplicity/Freertos_semaphore/src/main.c
@@ -0,0 +1,154 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Màrius Montón <marius.monton@gmail.com>
+ * @version V1.0
+ * @date    25-July-2017
+ * @brief   Basic FreeRTOS Semaphore example
+ * @license This project is released under the GNU Public License.s
+ *
+ * ******************************************************************************
+ */
+
+/** @addtogroup main
+ * @brief main file
+ * @{
+ */
+
+/* STD libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* FreeRTOS includes */
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+/* emlib includes */
+#include "em_chip.h"
+
+/* emdrv includes */
+#include "sleep.h"
+
+/* our includes */
+#include "BSP.h"
+
+/** Priority for the ToogleLed Task*/
+#define TOGGLE_TASK_PRIORITY          (tskIDLE_PRIORITY + 1)
+
+/** @brief binary semaphore for button 0 */
+SemaphoreHandle_t semaphore_button_0;
+
+/** @brief binary semaphore for button 0 */
+SemaphoreHandle_t semaphore_button_1;
+
+/**
+ * @brief Button 0 ISR
+ *
+ * Gives corresponding binary semaphore every time it enters
+ */
+void GPIO_EVEN_IRQHandler(void) {
+	uint32_t aux;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	/* clear flags */
+	aux = GPIO_IntGet();
+	GPIO_IntClear(aux);
+
+	/* Toggle semaphore */
+	xSemaphoreGiveFromISR(semaphore_button_0, &xHigherPriorityTaskWoken);
+
+	portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+/**
+ * @brief Button 1 ISR
+ *
+ * Gives corresponding binary semaphore every time it enters
+ */
+void GPIO_ODD_IRQHandler(void) {
+	uint32_t aux;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	/* clear flags */
+	aux = GPIO_IntGet();
+	GPIO_IntClear(aux);
+
+	/* Toggle semaphore */
+	xSemaphoreGiveFromISR(semaphore_button_1, &xHigherPriorityTaskWoken);
+
+	portEND_SWITCHING_ISR(xHigherPriorityTaskWoken);
+}
+
+/**
+ * @brief Our first task. A task is always a endless loop
+ *
+ * @param pParameter Unused parameter
+ * @return none
+ *
+ */
+static void TaskLedToggle(void *pParameter) {
+
+	(void) pParameter;
+
+	/* Here optional init variables or functions */
+
+	for (;;) {
+		xSemaphoreTake(semaphore_button_0, portMAX_DELAY);
+		LedToggle();
+	}
+}
+
+/**
+ * @brief main function
+ */
+int main(void) {
+	/* Chip errata */
+	CHIP_Init();
+
+	/* Init functions for our BSP */
+	BSP_Init();
+
+	/* Init SLEEP library */
+	SLEEP_Init(NULL, NULL);
+#if (configSLEEP_MODE < 3)
+	SLEEP_SleepBlockBegin((SLEEP_EnergyMode_t) (configSLEEP_MODE + 1));
+#endif
+
+	/* Create semaphores */
+	semaphore_button_0 = xSemaphoreCreateBinary();
+	semaphore_button_1 = xSemaphoreCreateBinary();
+
+	/* Create our first task */
+	xTaskCreate(TaskLedToggle, (const char *) "LedToggle",
+	configMINIMAL_STACK_SIZE, NULL, TOGGLE_TASK_PRIORITY, NULL);
+
+	/* Start FreeRTOS Scheduler */
+	vTaskStartScheduler();
+
+	return 0;
+}
+
+/*
+ * @}
+ */
+
+/**
+ * @mainpage FreeRTOS basic semaphore example
+ *
+ *
+ * This examples uses a binary semaphore to signal from a ISR to a task
+ *
+ * The ISR is attached to button 0 IRQ and "gives" a binary semaphore
+ *
+ * The task is trying to "take" the binary semaphore and when it does,
+ * just toggles the led
+ *
+ *
+ *
+ * This project uses the following libraries:
+ * @li @ref BSP
+ *
+ * @}
+ */
